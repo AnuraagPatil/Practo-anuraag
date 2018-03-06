7a001565b5dc176879da107e5ebdb75c93ffc62b
Anuraag <Anuraag0551@gmail.com>
2018-03-06 16:02:20 +0530

    Get doctors list to appoint

--- app/src/main/java/com/sourcekode/practo/practo/Doctors.java
+++ app/src/main/java/com/sourcekode/practo/practo/Doctors.java
@@ -3,7 +3,11 @@ package com.sourcekode.practo.practo;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
+
+import com.sourcekode.practo.practo.SampleDataProvider.DataProvider;
 
 public class Doctors extends AppCompatActivity {
 
@@ -14,6 +18,11 @@ public class Doctors extends AppCompatActivity {
 
         String Speciality = getIntent().getStringExtra(RecyclerViewAdapter.SPECILITY_KEY);
         setTitle(Speciality);
+
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerview_doc_list);
+        RecyclerViewAdapterForDocList myAdapter = new RecyclerViewAdapterForDocList(this, DataProvider.doctorsList);
+        recyclerView.setLayoutManager(new GridLayoutManager(this,1));
+        recyclerView.setAdapter(myAdapter);
 
         Toast.makeText(this, "Speciality : "+Speciality, Toast.LENGTH_SHORT).show();
     }
